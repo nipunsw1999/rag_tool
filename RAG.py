97d28387ce0c0dbe93140ee8f1d3e0edd73e3414
n_i_p_u_n <112545976+nipunsw1999@users.noreply.github.com>
2025-03-12 21:02:13 +0530

    Updated

--- RAG.py
+++ RAG.py
@@ -3,6 +3,20 @@ from tools import arxiv, wiki_tool, llm
 from langchain import hub
 from langchain.agents import create_openai_tools_agent
 from langchain.agents import AgentExecutor
+from dotenv import load_dotenv
+import os
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import  FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.tools.retriever import create_retriever_tool 
+import streamlit as st 
+from langchain_community.utilities import ArxivAPIWrapper
+from langchain_community.tools import ArxivQueryRun
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 st.write("This is an Agentic RAG application powered by Wikipedia, ArXiv, Google Scholar, and custom-added websites for enhanced information retrieval.")
 
@@ -15,6 +29,20 @@ if "tools" not in st.session_state:
 
 if "url" not in st.session_state:
     st.session_state.url = ""
+
+if st.session_state.url != "":
+    loader = WebBaseLoader(st.session_state.url)
+    docs=loader.load()
+    documents = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200).split_documents(docs)
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vectorDB = FAISS.from_documents(documents, embeddings) 
+    retriever = vectorDB.as_retriever()
+    retriever_tool = create_retriever_tool(
+    retriever,
+    "url_search",
+    "",
+    )
+
 
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
@@ -37,7 +65,7 @@ with st.sidebar:
 
         link1 = st.text_input("Enter the URL: ")
         add_link = st.form_submit_button("Add Link")
-        if add_link and link1:
+        if add_link and link1!="":
             st.session_state.url = link1
             st.success("Link added")
 
@@ -86,3 +114,4 @@ if user_input:
     
     with st.chat_message("assistant"):
         st.write(response)
+
--- tools.py
+++ tools.py
@@ -22,6 +22,9 @@ os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 if "url" not in st.session_state:
     st.session_state.url = ""
 
+if "base_model" not in st.session_state:
+    st.session_state.base_model = "gemma2-9b-it"
+
 api_wrapper = WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=200)
 wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
 
@@ -29,7 +32,7 @@ arxiv_wrapper = ArxivAPIWrapper(top_k_results=1,doc_content_chars_max=200)
 arxiv=ArxivQueryRun(api_wrapper=arxiv_wrapper)
 
 if st.session_state.url != "":
-    loader = WebBaseLoader("https://docs.smith.langchain.com/")
+    loader = WebBaseLoader(st.session_state.url)
     docs=loader.load()
     documents = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200).split_documents(docs)
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
@@ -44,4 +47,7 @@ if st.session_state.url != "":
 
 
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.9)
+from langchain_groq import ChatGroq
+
+
+llm = ChatGroq(model_name=st.session_state.base_model, temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"))
